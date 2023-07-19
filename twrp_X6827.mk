25323ce4ced424c203e59856073b48ad47f89a7a
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-07-19 18:57:32 +0530

    Reduce size.

--- twrp_X6827.mk
+++ twrp_X6827.mk
@@ -13,21 +13,18 @@ $(call inherit-product-if-exists, vendor/pb/config/common.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Virtual A/B OTA
 # https://source.android.com/docs/core/ota/virtual_ab/implement#build-flags
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Enable updating of APEXes
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Device identifier.
 PRODUCT_DEVICE := X6827
@@ -36,5 +33,3 @@ PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := HOT 20S
 PRODUCT_MANUFACTURER := infinix
 PRODUCT_GMS_CLIENTID_BASE := android-$(PRODUCT_RRAND)
-
-
